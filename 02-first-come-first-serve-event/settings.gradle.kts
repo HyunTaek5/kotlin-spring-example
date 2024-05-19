335bfb54ec1e3544d3e589fe132cd03bec5a049f
HyunTaek Oh <oht222@naver.com>
2024-05-20 01:28:06 +0900

    init: 02-선착순-이벤트-시스템 기본 셋업

---
+++ 02-first-come-first-serve-event/settings.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+rootProject.name = "02-first-come-first-serve-event"
+
---
+++ 02-first-come-first-serve-event/src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package org.dku
+
+fun main() {
+  println("Hello World!")
+}
