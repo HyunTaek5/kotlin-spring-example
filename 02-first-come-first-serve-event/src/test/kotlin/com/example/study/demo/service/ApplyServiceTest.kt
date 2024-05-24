606a9fff2c5722cd55ef5c950fa8b637cf8d195a
HyunTaek Oh <oht222@naver.com>
2024-05-24 18:20:08 +0900

    section1: 동시성 테스트 작성

--- 02-first-come-first-serve-event/src/test/kotlin/com/example/study/demo/service/ApplyServiceTest.kt
+++ 02-first-come-first-serve-event/src/test/kotlin/com/example/study/demo/service/ApplyServiceTest.kt
@@ -1,6 +1,9 @@
 package com.example.study.demo.service
 
 import com.example.study.demo.repository.CouponRepository
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,5 +21,29 @@ class ApplyServiceTest(
     val count = couponRepository.count()
 
     assertThat(count).isEqualTo(1)
+  }
+
+  @Test
+  fun 여러명_응모() {
+    val threadCount = 1000
+    val executorService: ExecutorService = Executors.newFixedThreadPool(32)
+    val latch: CountDownLatch = CountDownLatch(threadCount)
+
+    for (i in 1..threadCount) {
+      val userId = i.toLong()
+      executorService.submit {
+        try {
+          applyService.apply(userId)
+        } finally {
+          latch.countDown()
+        }
+      }
+    }
+
+    latch.await()
+
+    val count = couponRepository.count()
+
+    assertThat(count).isEqualTo(100)
   }
 }
