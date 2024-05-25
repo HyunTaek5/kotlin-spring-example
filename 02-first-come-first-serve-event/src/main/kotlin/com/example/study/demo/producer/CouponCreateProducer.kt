905193b0a16be2f92a29f13399bc3aa49061b92a
HyunTaek Oh <oht222@naver.com>
2024-05-25 18:48:59 +0900

    section3: kafka producer 추가

---
+++ 02-first-come-first-serve-event/src/main/kotlin/com/example/study/demo/producer/CouponCreateProducer.kt
@@ -0,0 +1,11 @@
+package com.example.study.demo.producer
+
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+
+@Component
+class CouponCreateProducer(private val kafkaTemplate: KafkaTemplate<String, Long>) {
+  fun create(userId: Long) {
+    kafkaTemplate.send("coupon_create", userId)
+  }
+}
--- 02-first-come-first-serve-event/src/main/kotlin/com/example/study/demo/service/ApplyService.kt
+++ 02-first-come-first-serve-event/src/main/kotlin/com/example/study/demo/service/ApplyService.kt
@@ -1,6 +1,7 @@
 package com.example.study.demo.service
 
 import com.example.study.demo.domain.Coupon
+import com.example.study.demo.producer.CouponCreateProducer
 import com.example.study.demo.repository.CouponCountRepository
 import com.example.study.demo.repository.CouponRepository
 import org.springframework.stereotype.Service
@@ -8,19 +9,20 @@ import org.springframework.stereotype.Service
 @Service
 class ApplyService(
     private val couponRepository: CouponRepository,
-    private val couponCountRepository: CouponCountRepository
+    private val couponCountRepository: CouponCountRepository,
+    private val couponCreateProducer: CouponCreateProducer
 ) {
   fun apply(userId: Long) {
     val count = couponCountRepository.increment()
 
-      if (count != null) {
-          if (count > 100) {
-              return
-          }
+    if (count != null) {
+      if (count > 100) {
+        return
       }
+    }
 
     val coupon = Coupon(userId = userId)
 
-    couponRepository.save(coupon)
+    couponCreateProducer.create(userId)
   }
 }
