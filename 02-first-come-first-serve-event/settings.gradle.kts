e02c0374a48592e96c4d6213cecd3d95537bc03a
HyunTaek Oh <oht222@naver.com>
2024-05-25 20:25:15 +0900

    section3: kafka consumer 모듈 추가하기

--- 02-first-come-first-serve-event/settings.gradle.kts
+++ 02-first-come-first-serve-event/settings.gradle.kts
@@ -1,2 +1,8 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
 rootProject.name = "02-first-come-first-serve-event"
 
+include("api")
+include("consumer")
